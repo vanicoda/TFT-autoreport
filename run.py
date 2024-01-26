f224f4f0f08d8d857241c09f3fcd1299c99c54c9
vanicoda <vanicoda@gmail.com>
2024-01-26 20:59:55 +0900

    로직완성

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #외부 라이브러리
 import pyautogui as gui
 import time
+import pyperclip
 
 #모듈
 from checkwindow import check_window
@@ -9,6 +10,8 @@ import gui_window
 #상수
 GAME_TITLE = "League of Legends"
 CLIENT_RES = (1600,900)
+CHECKBOX_IMAGE_PATH = "./image/checkbox.png" # 참고 : 24x24 이미지임
+REPORT_IMAGE_PATH = "./image/repbtn.png" # 참고 : 148x30 이미지
 
 ####################################################
 
@@ -25,14 +28,52 @@ else:
   exit()
 
 # 2. GUI에서 체크한 n1, n2, n3번째 체크박스에 체크
+
+def report_process():
+  try:
+    gui.alert(title="TFT Autoreport", text="자동 리폿을 시작합니다.")
+    time.sleep(0.1)
+    gui.getWindowsWithTitle(GAME_TITLE)[0].activate()
+    time.sleep(0.3)  
+
+    #이미지서칭
+    checkbox_images = list(gui.locateAllOnScreen(CHECKBOX_IMAGE_PATH))
+
+    first_checkbox_top = checkbox_images[0].top
+    first_checkbox_left = checkbox_images[0].left
+    
+    print(first_checkbox_top, first_checkbox_left)
+    
+    checkboxes_top = []
+    for i in range(4):
+      checkboxes_top.append(first_checkbox_top + (i*55)) #참고 : 체크박스 세로간격이 55px임
+
+    print(checkboxes_top)
+
+    # 체크박스 클릭
+    for i in range(4):
+      if rep_data[i]:
+        gui.click(x=first_checkbox_left + 12, y=checkboxes_top[i] + 12, duration=0.15)
+
+    # 리폿 내용 입력(복붙)
+    time.sleep(0.5)
+    gui.click(x=first_checkbox_left + 24, y=checkboxes_top[3] + 50) #대충 이쯤에 리폿내용 적는 상자
+    time.sleep(0.5)
+    pyperclip.copy(rep_data[4])
+    gui.hotkey("ctrl", "v")
+    time.sleep(0.5)
+
+    # 리폿 버튼 클릭
+    repbtn = gui.locateOnScreen(REPORT_IMAGE_PATH)
+    gui.click(x=repbtn.left + 74, y=repbtn.top + 15)
+    
+
+  except Exception as e:
+    gui.alert(title="오류", text=f"이미지 인식 오류거나, 다른 오류가 발생한듯? \n오류내용: <{e}>")
+
 if gui_done:
-  gui.alert(title="TFT Autoreport", text="자동 리폿을 시작합니다.")
-  time.sleep(0.5)
-  gui.getWindowsWithTitle(GAME_TITLE)[0].activate()
-  
-  checkbox_images = list(gui.locateAllOnScreen("./image/checkbox.png"))
-  print(checkbox_images[0])
-  # gui.click(checkbox_image, duration=0.1)
+  report_process()
+
 
 # 3. 문구입력창 감지 후 GUI에서 입력해둔 문구 입력
 # 4. 리폿버튼 감지 후 클릭
