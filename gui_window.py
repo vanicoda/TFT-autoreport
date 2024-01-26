614b53bd43cc7237d222208299074f1c1fa2edbb
vanicoda <vanicoda@gmail.com>
2024-01-26 19:42:52 +0900

    gui 진짜완성, image추가, 이미지서칭

--- gui_window.py
+++ gui_window.py
@@ -1,81 +1,85 @@
 import tkinter as tk
 import tkinter.messagebox
 
-# Tkinter 창
-window = tk.Tk()
-window.title("TFT Autoreport")
-window.geometry("350x450")
-
-# 체크박스 변수 및 라벨 변수
-var_check_1 = tk.BooleanVar()
-var_check_2 = tk.BooleanVar()
-var_check_3 = tk.BooleanVar()
-var_check_4 = tk.BooleanVar()
-label_var = tk.StringVar(value="리폿할 항목을 선택해주세요")
-
-# 체크박스
-def on_checkbox_click():
-    label_text = ""
-    if var_check_1.get():
-        label_text += "욕설 "
-    if var_check_2.get():
-        label_text += "혐오발언 "
-    if var_check_3.get():
-        label_text += "부정행위 "
-    if var_check_4.get():
-        label_text += "부적절이름"
-
-    label_var.set(label_text)
-
-checkbox_1 = tk.Checkbutton(window, text="욕설", variable=var_check_1, command=on_checkbox_click)
-checkbox_1.pack(pady=5)
-checkbox_2 = tk.Checkbutton(window, text="혐오 발언", variable=var_check_2, command=on_checkbox_click)
-checkbox_2.pack(pady=5)
-checkbox_3 = tk.Checkbutton(window, text="부정행위(핵 사용)", variable=var_check_3, command=on_checkbox_click)
-checkbox_3.pack(pady=5)
-checkbox_4 = tk.Checkbutton(window, text="불쾌감을 주거나 부적절한 이름 사용", variable=var_check_4, command=on_checkbox_click)
-checkbox_4.pack(pady=5)
-
-# 체크박스 상태 확인 라벨 
-
-label = tk.Label(window, textvariable=label_var)
-label.config(fg="red")
-label.pack()
-
-
-# 텍스트 상자
-
-textarea_label = tk.Label(window, text="리폿 내용(최대 250자)")
-textarea_label.pack(pady=30)
-
-textarea = tk.Text(window, height=8, width=40)
-textarea.pack()
-
-#버튼
-
-def button_onclick():
+def gui_main():
     #리폿항목 초기화
     report_data = []
-    #체크박스 체크된지 확인(if not - alert)
-    if var_check_1.get() or var_check_2.get() or var_check_3.get() or var_check_4.get():
-        #리폿내용 가져오기
-        var_textarea = textarea.get("1.0", "end-1c")[:250]
 
-        report_data.append(var_check_1.get())
-        report_data.append(var_check_2.get())
-        report_data.append(var_check_3.get())
-        report_data.append(var_check_4.get())
-        report_data.append(var_textarea)
+    # Tkinter 창
+    window = tk.Tk()
+    window.title("TFT Autoreport")
+    window.geometry("350x450")
 
-    #창 닫기
-        window.destroy()
-        print(report_data)
-        return report_data # 여기서부터 시작하기
-    else:
-        tkinter.messagebox.showerror("오류", "리폿 항목을 체크해주세요")
+    # 체크박스 변수 및 라벨 변수
+    var_check_1 = tk.BooleanVar()
+    var_check_2 = tk.BooleanVar()
+    var_check_3 = tk.BooleanVar()
+    var_check_4 = tk.BooleanVar()
+    label_var = tk.StringVar(value="리폿할 항목을 선택해주세요")
 
-button = tk.Button(window, text="자동 리폿 시작", command=button_onclick)
-button.pack(pady=10)
+    # 체크박스
+    def on_checkbox_click():
+        label_text = ""
+        if var_check_1.get():
+            label_text += "욕설 "
+        if var_check_2.get():
+            label_text += "혐오발언 "
+        if var_check_3.get():
+            label_text += "부정행위 "
+        if var_check_4.get():
+            label_text += "부적절이름"
 
-# 프로그램 실행
-window.mainloop()
+        label_var.set(label_text)
+
+    checkbox_1 = tk.Checkbutton(window, text="욕설", variable=var_check_1, command=on_checkbox_click)
+    checkbox_1.pack(pady=5)
+    checkbox_2 = tk.Checkbutton(window, text="혐오 발언", variable=var_check_2, command=on_checkbox_click)
+    checkbox_2.pack(pady=5)
+    checkbox_3 = tk.Checkbutton(window, text="부정행위(핵 사용)", variable=var_check_3, command=on_checkbox_click)
+    checkbox_3.pack(pady=5)
+    checkbox_4 = tk.Checkbutton(window, text="불쾌감을 주거나 부적절한 이름 사용", variable=var_check_4, command=on_checkbox_click)
+    checkbox_4.pack(pady=5)
+
+    # 체크박스 상태 확인 라벨 
+
+    label = tk.Label(window, textvariable=label_var)
+    label.config(fg="red")
+    label.pack()
+
+
+    # 텍스트 상자
+
+    textarea_label = tk.Label(window, text="리폿 내용(최대 250자)")
+    textarea_label.pack(pady=30)
+
+    textarea = tk.Text(window, height=8, width=40)
+    textarea.pack()
+
+    #버튼
+
+    def button_onclick():
+        #체크박스 체크된지 확인
+        if var_check_1.get() or var_check_2.get() or var_check_3.get() or var_check_4.get():
+            #리폿내용 가져오기
+            var_textarea = textarea.get("1.0", "end-1c")[:250]
+
+            report_data.append(var_check_1.get())
+            report_data.append(var_check_2.get())
+            report_data.append(var_check_3.get())
+            report_data.append(var_check_4.get())
+            report_data.append(var_textarea)
+
+        #창 닫기
+            window.destroy()
+        else:
+            tkinter.messagebox.showerror("오류", "리폿 항목을 체크해주세요")
+
+    button = tk.Button(window, text="자동 리폿 시작", command=button_onclick)
+    button.pack(pady=10)
+
+    # 프로그램 실행
+    window.mainloop()
+    
+    # 프로그램 종료
+    if report_data is not []:
+        return report_data
--- run.py
+++ run.py
@@ -1,17 +1,40 @@
+#외부 라이브러리
 import pyautogui as gui
+import time
+
+#모듈
+from checkwindow import check_window
+import gui_window
+
+#상수
+GAME_TITLE = "League of Legends"
+CLIENT_RES = (1600,900)
+
+####################################################
 
 # 1. Leagueclient.exe 켜져있는지 감지하고 GUI window 실행, if not - alert
-is_running = False
 rep_data = []
-from checkwindow import check_window
-if check_window("League of Legends", (1600,900)):
-  import gui_window
+gui_done = False
+
+if check_window(GAME_TITLE,CLIENT_RES):
+  rep_data = gui_window.gui_main()
+  print(rep_data)
+  gui_done = True
 else:
   gui.alert(title="실행 불가",text="1. 클라이언트가 실행 중이 아니거나 \n2. 최소화되었거나 \n3. 해상도가 1600*900 이 아님")
   exit()
-# 3. GUI에서 체크한 n1, n2, n3번째 체크박스에 체크
 
-# 4. 문구입력창 감지 후 GUI에서 입력해둔 문구 입력
-# 5. 리폿버튼 감지 후 클릭
-# 6. 기다리기...(client lag)
-# 7. if [체크박스 감지됨] 이라면 2~6번 반복, if not - loop 종료 후 alert 출력
+# 2. GUI에서 체크한 n1, n2, n3번째 체크박스에 체크
+if gui_done:
+  gui.alert(title="TFT Autoreport", text="자동 리폿을 시작합니다.")
+  time.sleep(0.5)
+  gui.getWindowsWithTitle(GAME_TITLE)[0].activate()
+  
+  checkbox_images = list(gui.locateAllOnScreen("./image/checkbox.png"))
+  print(checkbox_images[0])
+  # gui.click(checkbox_image, duration=0.1)
+
+# 3. 문구입력창 감지 후 GUI에서 입력해둔 문구 입력
+# 4. 리폿버튼 감지 후 클릭
+# 5. 기다리기...(client lag)
+# 6. if [체크박스 감지됨] 이라면 2~6번 반복, if not - loop 종료 후 alert 출력
